6351a0b0e5023bcf74fdef3af0afda3588cb03ab
Igor Owczarek <UL0253578@edu.uni.lodz.pl>
2022-06-16 19:03:28 +0200

    Zmiany w funkcjach

--- skrypt_tworzacy_funckcje.sql
+++ skrypt_tworzacy_funckcje.sql
@@ -22,13 +22,34 @@
 --	RETURN @data_zwrotu
 --END
 
-CREATE OR ALTER FUNCTION uf_czyZwrocicKaucje(@id_wypozyczenia int)
-RETURNS money
-BEGIN 
-DECLARE @kaucja money
-	IF (SELECT StanTechniczny FROM tbl_wypozyczenie WHERE IdWypozyczenia = @id_wypozyczenia) = 'dobry'
-		SET @kaucja = 0
-	ELSE
-		SET @kaucja = 1000
-RETURN @kaucja
-END
+--CREATE OR ALTER FUNCTION uf_czyZwrocicKaucje(@id_wypozyczenia int)
+--RETURNS money
+--BEGIN 
+--DECLARE @kaucja money
+--	IF (SELECT StanTechniczny FROM tbl_wypozyczenie WHERE IdWypozyczenia = @id_wypozyczenia) = 'dobry'
+--		SET @kaucja = 0
+--	ELSE
+--		SET @kaucja = 1000
+--RETURN @kaucja
+--END
+--CREATE OR ALTER FUNCTION uf_getCarId(@id_faktury int)
+--RETURNS int
+--BEGIN
+--DECLARE @id int
+--	SET @id = (SELECT IdSamochodu FROM tbl_rezerwacja r INNER JOIN tbl_faktura f ON r.IdRezerwacji = f.IdRezerwacji WHERE IdFaktury=@id_faktury) 
+--RETURN @id
+--END
+--CREATE OR ALTER FUNCTION uf_getCarType(@id_faktury int)
+--RETURNS tinyint
+--BEGIN
+--	DECLARE @id int
+--	SET @id = (SELECT IdRodzajuSamochodu FROM tbl_samochod s INNER JOIN tbl_rezerwacja r ON r.IdSamochodu = s.IdSamochodu INNER JOIN tbl_faktura f ON f.IdRezerwacji = r.IdRezerwacji WHERE IdFaktury = @id_faktury)
+--RETURN @id
+--END
+--CREATE OR ALTER FUNCTION uf_getLacznaKwota(@id_faktury int)
+--RETURNS money
+--BEGIN
+--DECLARE @laczna_kwota money
+--	SET @laczna_kwota = (SELECT LacznaKwotaDoZaplaty FROM tbl_rezerwacja r INNER JOIN tbl_faktura f ON r.IdRezerwacji = f.IdRezerwacji WHERE IdFaktury=@id_faktury) 
+--RETURN @laczna_kwota
+--END
