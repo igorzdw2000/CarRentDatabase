5e464d1bde7b3ea8b6af3e93310f3e9e9fd23e80
Igor Owczarek <UL0253578@edu.uni.lodz.pl>
2022-06-17 15:07:10 +0200

    procedury dodania wypozyczenia i rezerwacji

--- skrypt_tworzacy_procedury.sql
+++ skrypt_tworzacy_procedury.sql
@@ -133,3 +133,89 @@ GO
 --	EXECUTE usp_GetErrorInfo
 --END CATCH
 	
+CREATE SEQUENCE new_id_rezerwacja
+INCREMENT BY 11
+START WITH 100
+NO CYCLE
+GO
+
+CREATE OR ALTER PROCEDURE up_dodaj_rezerwacje
+@id_wypozyczenia int,
+@id_samochodu int,
+@data_utworzenia_rezerwacji date
+AS
+BEGIN TRY
+	DECLARE @msg varchar(70)
+	IF @id_wypozyczenia IS NULL OR
+	@id_samochodu IS NULL OR
+	@data_utworzenia_rezerwacji IS NULL
+	BEGIN
+	SET @msg = 'Wartoœci nie mog¹ przyjmowaæ NULL'
+	RAISERROR(@msg,16,1)
+	END
+	IF (SELECT IdWypozyczenia FROM tbl_wypozyczenie WHERE IdWypozyczenia = @id_wypozyczenia) IS NULL
+	BEGIN
+	SET @msg = 'Brak wypo¿yczenia o podanym numerze, dodaj nowe wypo¿yczenie'
+	RAISERROR(@msg,16,1)
+	END
+	IF (SELECT dbo.uf_czy_wolny_samochod(@id_samochodu)) = 1
+	BEGIN
+	INSERT INTO tbl_rezerwacja(IdRezerwacji,IdWypozyczenia,IdSamochodu,CenaZaDobe,Kaucja,DataRezerwacji,DataKoncaRezerwacji,DataUtworzeniaRezerwacji,LacznaKwotaDoZaplaty)
+	VALUES(NEXT VALUE FOR new_id_rezerwacja,@id_wypozyczenia,@id_samochodu,dbo.uf_getCar(@id_samochodu),1000,dbo.uf_getDataWypozyczenia(@id_wypozyczenia),dbo.uf_getDataZwrotu(@id_wypozyczenia),@data_utworzenia_rezerwacji,(dbo.uf_czyZwrocicKaucje(@id_wypozyczenia)+CONVERT(money,dbo.uf_getCar(@id_samochodu)*DATEDIFF(day,dbo.uf_getDataWypozyczenia(@id_wypozyczenia),dbo.uf_getDataZwrotu(@id_wypozyczenia)))))
+	END
+	ELSE
+	BEGIN
+	SET @msg = 'Podany samochód jest niedostêpny'
+	RAISERROR(@msg,16,1)
+	END
+END TRY
+BEGIN CATCH
+DECLARE @ErrorMessage NVARCHAR(4000);  
+    DECLARE @ErrorSeverity INT;  
+    DECLARE @ErrorState INT;  
+  
+    SELECT   
+        @ErrorMessage = ERROR_MESSAGE(),  
+        @ErrorSeverity = ERROR_SEVERITY(),  
+        @ErrorState = ERROR_STATE();  
+	RAISERROR(@ErrorMessage,@ErrorSeverity,@ErrorState)
+END CATCH
+GO
+
+CREATE SEQUENCE new_id_wypozyczenie
+INCREMENT BY 11
+START WITH 100
+NO CYCLE
+GO
+
+CREATE OR ALTER PROCEDURE up_dodaj_wypozyczenie
+@data_wypozyczenia date,
+@data_zwrotu date,
+@stan_techniczny varchar(100)
+AS
+BEGIN TRY
+	DECLARE @msg varchar(70)
+	IF @data_wypozyczenia IS NULL OR
+	@data_zwrotu IS NULL OR
+	@stan_techniczny IS NULL 
+	BEGIN 
+	SET @msg = 'Wartoœci nie mog¹ przyjmowaæ wartoœci NULL'
+	RAISERROR(@msg,16,1)
+	END
+	ELSE
+	BEGIN
+	INSERT INTO tbl_wypozyczenia(IdWypozyczenia, DataWypozyczenia, DataZwrotu, StanTechniczny)
+	VALUES(NEXT VALUE FOR new_id_wypozyczenie,@data_wypozyczenia,@data_zwrotu,@stan_techniczny)
+	END
+END TRY
+BEGIN CATCH
+	DECLARE @ErrorMessage NVARCHAR(4000);  
+    DECLARE @ErrorSeverity INT;  
+    DECLARE @ErrorState INT;  
+  
+    SELECT   
+        @ErrorMessage = ERROR_MESSAGE(),  
+        @ErrorSeverity = ERROR_SEVERITY(),  
+        @ErrorState = ERROR_STATE();  
+	RAISERROR(@ErrorMessage,@ErrorSeverity,@ErrorState)
+END CATCH
