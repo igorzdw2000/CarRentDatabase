3c9696c32a73a03907637a4ecd88d272f3c11ecc
Igor Owczarek <UL0253578@edu.uni.lodz.pl>
2022-06-17 13:41:50 +0200

    procedura dodania samochodu

--- skrypt_tworzacy_procedury.sql
+++ skrypt_tworzacy_procedury.sql
@@ -55,3 +55,51 @@ END TRY
 BEGIN CATCH
 	EXECUTE usp_GetErrorInfo 
 END CATCH
+GO
+CREATE SEQUENCE new_id_samochod
+INCREMENT BY 11
+START WITH 100
+NO CYCLE
+GO
+
+CREATE PROCEDURE up_dodaj_auto
+@rejestracja varchar(15),
+@model varchar(50),
+@marka varchar(50),
+@rodzaj_nadwozia varchar(25),
+@silnik varchar(50),
+@rodzaj_samochodu varchar(25),
+@kolor varchar(30),
+@przebieg int,
+@rocznik int,
+@vin varchar(17)
+AS
+BEGIN TRY
+	DECLARE @msg varchar(50)
+	IF @rejestracja IS NULL OR
+	@model IS NULL OR
+	@marka IS NULL OR
+	@rodzaj_nadwozia IS NULL OR
+	@silnik IS NULL OR
+	@rodzaj_samochodu IS NULL OR
+	@kolor IS NULL OR
+	@przebieg IS NULL OR
+	@rocznik IS NULL OR
+	@vin IS NULL 
+	BEGIN
+	SET @msg = 'Kolumny nie mog¹ przyjmowaæ wartoœci NULL'
+	END
+	BEGIN
+	DECLARE @id_model int, @id_marka int, @id_rodzaj_nadwozia int, @id_rodzaj_samochodu int, @id_silnika int
+	SET @id_model = (SELECT IdModelu FROM dict.tbl_model WHERE Model = @model)
+	SET @id_marka = (SELECT IdMarki FROM dict.tbl_marka WHERE Marka = @marka)
+	SET @id_rodzaj_nadwozia = (SELECT IdRodzajuNadwozia FROM dict.tbl_rodzaj_nadwozia WHERE RodzajNadwozia = @rodzaj_nadwozia)
+	SET @id_rodzaj_samochodu = (SELECT IdRodzajuSamochodu FROM dict.tbl_rodzaj_samochodu WHERE RodzajSamochodu = @rodzaj_samochodu)
+	SET @id_silnika = (SELECT IdSilnika FROM dict.tbl_silnik WHERE TypSilnika=@silnik)
+	INSERT INTO tbl_samochod(IdSamochodu,Rejestracja,IdModelu,IdMarki,IdRodzajuNadwozia,IdSilnika,IdRodzajuSamochodu,Kolor,Przebieg,Rocznik,VIN)
+	VALUES(NEXT VALUE FOR new_id_samochod,@rejestracja,@id_model,@id_marka,@id_rodzaj_nadwozia,@id_silnika,@id_rodzaj_samochodu,@kolor,@przebieg,@rocznik,@vin)
+	END
+END TRY
+BEGIN CATCH
+	EXECUTE usp_GetErrorInfo 
+END CATCH
